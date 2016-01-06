acc42faa5c4d7daacaa4387a4e3106b205643de1
Francois Hill (fhill-wks) <francoishill11@gmail.com>
2016-01-06 16:03:19 +0200

    Better feedback in panic when accurev command fails.

--- run_command.go
+++ run_command.go
@@ -8,7 +8,7 @@ import (
 func mustRunAccurevCommand(args ...interface{}) []byte {
 	output, err := sh.Command("accurev", args...).CombinedOutput()
 	if err != nil {
-		panic(fmt.Sprintf("Cannot execute `accurev %#v`, %s", args, err.Error()))
+		panic(fmt.Sprintf("Cannot execute `accurev %#v`, %s. Output: %s", args, err.Error(), string(output)))
 	}
 	return output
 }
